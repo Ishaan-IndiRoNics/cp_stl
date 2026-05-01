77054aadf8952cb81fc98a25a4f315e81d4fb4b3
Ishaan-IndiRoNics <ishaan31211@gmail.com>
2026-05-01 14:20:52 +0530

    Add files via upload

---
+++ __stl/dsa/str_hash.cpp
@@ -0,0 +1,27 @@
+#define A 911382323LL
+#define B 1000000007LL
+#define C 972663749LL
+#define D 1000000009LL
+
+vll h1, p1, h2, p2;
+
+void init(string s, int n) {
+	h1.assign(n, 0);
+	p1.assign(n, 0);
+	h2.assign(n, 0);
+	p2.assign(n, 0);
+	h1[0] = s[0];
+	p1[0] = 1;
+	h2[0] = s[0];
+	p2[0] = 1;
+	for (int i = 1; i < n; i++) {
+		h1[i] = (h1[i-1]*A+s[i])%B;
+		p1[i] = (p1[i-1]*A)%B;
+		h2[i] = (h2[i-1]*C+s[i])%D;
+		p2[i] = (p2[i-1]*C)%D;
+	}
+}
+
+pair<ll,ll> hash(int a, int b) {
+	return (a==0)?mp(h1[b], h2[b]):mp((h1[b]-(h1[a-1]*p1[b-a+1])%B+B)%B,(h2[b]-(h2[a-1]*p2[b-a+1])%D+D)%D);
+}
